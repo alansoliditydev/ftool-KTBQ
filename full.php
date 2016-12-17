4ed4dfc564f43be8d1abbb03349909bb481e205f
khangtd <khangtd.09@gmail.com>
2016-12-17 16:48:03 +0700

    update source

--- full.php
+++ full.php
@@ -1,28 +1,42 @@
 <?php
 //Choiem1lan2 :v
-if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["app_id"])){
-$email = $_POST['email'];
-$pass = $_POST['password'];
-$app_id = $_POST["app_id"];
-if($app_id == 350685531728)
-{
-$token = json_decode(file_get_contents('http://localhost/android.php?u='.$email.'&p='.$pass.''),true);
-if($token['access_token']) Echo $token['access_token'];
-else Echo $token['error_msg'];
-}else if($app_id == 165907476854626)
-{
-$token = json_decode(file_get_contents('http://localhost/ios.php?u='.$email.'&p='.$pass.''),true);
-if(isset($token['access_token'])) Echo $token['access_token'];
-else Echo $token['error_msg'];
-//echo $token;
-//Echo 'fucking-good';
-}else if($app_id == 6628568379)
-{
-$token = json_decode(file_get_contents('http://localhost/iphone.php?u='.$email.'&p='.$pass.''),true);
-if($token['access_token']) Echo $token['access_token'];
-else Echo $token['error_msg'];
+if ($_POST["account_list"] != '' && isset($_POST["app_id"])){
+  $app_id = $_POST["app_id"];
+
+  $accounts = preg_split ("/\r\n|\n|\r/",$_POST['account_list']);
+  $token_lists = '';
+
+    foreach($accounts as $account)
+    {
+      $access_info = explode('|',$account);
+      if (isset($access_info[0]) && isset($access_info[1]))
+      {
+      $email = $access_info[0];
+      $pass = $access_info[1];
+      if($app_id == 350685531728)
+        {
+          $token = json_decode(file_get_contents('http://localhost/android.php?u='.$email.'&p='.$pass.''),true);
+        }
+      else if($app_id == 165907476854626)
+        {
+          $token = json_decode(file_get_contents('http://localhost/ios.php?u='.$email.'&p='.$pass.''),true);
+        }
+      else if($app_id == 6628568379)
+        {
+          $token = json_decode(file_get_contents('http://localhost/iphone.php?u='.$email.'&p='.$pass.''),true);
+        }
+      if(isset($token['access_token'])) $token_lists .= $token['access_token'].PHP_EOL;
+      else $token_lists .= $token['error_msg'].PHP_EOL;
+    }
+    else {
+      $token_lists .= 'Please fill the form with correct format'.PHP_EOL;
+    }
 }
-
-
+  echo $token_lists;
+}
+else{
+# echo $_POST['account_list'];
+#echo $_POST['app_id'];
+echo 'Please fill in enough information';
 }
 ?>
--- index.php
+++ index.php
@@ -20,12 +20,10 @@
 </div>
 <div class="panel-body">
 <div class="form-group">
-<label for="email">* Email hoặc số điện thoại :</label>
-<input id="email" placeholder="Nhập Email" class="form-control"/>
+<label for="account">* Nhập thông tin account :</label>
+<textarea style='height: 200px;' id="account" placeholder="account1|password1&#10;account2|password2" class="form-control"></textarea>
 </div>
-<div class="form-group"><label for="password">* Mật khẩu :</label>
-<input id="password" type="password" placeholder="Nhập Mật Khẩu" class="form-control"/>
-</div>
+
 <div class="form-group"><label for="app_id">* Chọn Ứng Dụng :</label>
 <select id="app_id" class="form-control">
 <option value="6628568379">Facebook For Iphone</option>
@@ -33,28 +31,27 @@
 <option value="165907476854626">PAGES MANAGER FOR IOS</option>
 </select>
 </div>
+
 <div class="form-group text-center">
 <button id="submit" class="btn btn-sm btn-primary">Lấy Token</button>
 </div>
 <div class="form-group text-center" id="load_result" style="display:none">
 <label for="result">Access Token của bạn là :</label>
-<textarea id="result" onclick="this.focus();this.select()" class="form-control" rows="2"></textarea>
+<textarea readonly style='height: auto; min-height: 200px;' id="result" onclick="this.focus();this.select()" class="form-control" rows="2"></textarea>
 </div>
 </div>
 </div>
 <script>
-    var _0x6619 = ["click", "#submit", "disabled", "attr", "Anh em vui lòng đợi...", "html", "val", "#email", "#password", "#app_id option:selected", "removeAttr", "Lấy token", "show", "#load_result", "Thất bại vui lòng thử lại", "#result", "fail", "http://localhost/full.php", "post", "ajax", "on"];
+    var _0x6619 = ["click", "#submit", "disabled", "attr", "Anh em vui lòng đợi...", "html", "val", "#account", "#password", "#app_id option:selected", "removeAttr", "Lấy token", "show", "#load_result", "Thất bại vui lòng thử lại", "#result", "fail", "http://localhost/full.php", "post", "ajax", "on"];
     $(document)[_0x6619[20]](_0x6619[0], _0x6619[1], function () {
         $(_0x6619[1])[_0x6619[3]](_0x6619[2], _0x6619[2]), $(_0x6619[1])[_0x6619[5]](_0x6619[4]);
         var _0x1ea6x1 = $(_0x6619[7])[_0x6619[6]](),
-            _0x1ea6x2 = $(_0x6619[8])[_0x6619[6]](),
             _0x1ea6x3 = $(_0x6619[9])[_0x6619[6]]();
         $[_0x6619[19]]({
             url: _0x6619[17],
             type: _0x6619[18],
             data: {
-                email: _0x1ea6x1,
-                password: _0x1ea6x2,
+                account_list: _0x1ea6x1,
                 app_id: _0x1ea6x3
             },
             success: function (_0x1ea6x1) {
