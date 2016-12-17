afb201d996e39df5221be8f2121ba63c6cdba8b7
khangtd <khangtd.09@gmail.com>
2016-12-17 13:04:15 +0700

    1 trieu do

---
+++ full.php
@@ -0,0 +1,28 @@
+<?php
+//Choiem1lan2 :v
+if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["app_id"])){
+$email = $_POST['email'];
+$pass = $_POST['password'];
+$app_id = $_POST["app_id"];
+if($app_id == 350685531728)
+{
+$token = json_decode(file_get_contents('http://localhost/android.php?u='.$email.'&p='.$pass.''),true);
+if($token['access_token']) Echo $token['access_token'];
+else Echo $token['error_msg'];
+}else if($app_id == 165907476854626)
+{
+$token = json_decode(file_get_contents('http://localhost/ios.php?u='.$email.'&p='.$pass.''),true);
+if(isset($token['access_token'])) Echo $token['access_token'];
+else Echo $token['error_msg'];
+//echo $token;
+//Echo 'fucking-good';
+}else if($app_id == 6628568379)
+{
+$token = json_decode(file_get_contents('http://localhost/iphone.php?u='.$email.'&p='.$pass.''),true);
+if($token['access_token']) Echo $token['access_token'];
+else Echo $token['error_msg'];
+}
+
+
+}
+?>
