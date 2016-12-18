a1cc146338bb1a37667535214233efbb87557ab0
khangtd <khangtd.09@gmail.com>
2016-12-18 13:07:52 +0700

    finish detect bug

--- ios.php
+++ ios.php
@@ -21,6 +21,8 @@ $cnf['login'] = 'Login';
 $random = md5(rand(00000000,99999999)).'.txt';
 $login = cURL_iOS('https://m.facebook.com/login.php', $random, $cnf,$useragent);
 //print $login;
+$dom = new DOMDocument();
+
 if(preg_match('/name="fb_dtsg" value="(.*?)"/', $login, $response)){
     $fb_dtsg = $response[1];
     $responseToken = cURL_iOS('https://www.facebook.com/v1.0/dialog/oauth/confirm?', $random, 'fb_dtsg='.$fb_dtsg.'&app_id=165907476854626&redirect_uri=fbconnect://success&display=popup&access_token=&sdk=&from_post=1&private=&tos=&login=&read=&write=&extended=&social_confirm=&confirm=&seen_scopes=&auth_type=&auth_token=&auth_nonce=&default_audience=&ref=Default&return_format=access_token&domain=&sso_device=ios&__CONFIRM__=1',$useragent);
@@ -32,12 +34,26 @@ if(preg_match('/name="fb_dtsg" value="(.*?)"/', $login, $response)){
     }
     else
     {
-        $token['error_msg'] = $data['error'];
+        // Render HTML to get error
+        $error = $dom->loadHTML($responseToken);
+        $dom->validateOnParse = true;
+        $nodes = array();
+        $nodes = $dom->getElementsByTagName("input");
+        foreach ($nodes as $element)
+        {
+            $attr = $element->getAttribute("name");
+            if (strpos($attr, "error") >= 0)
+            {
+                $error_msg = $element->getAttribute('value');
+            }
+        }
+        //$token['error_msg'] = "Lỗi Facebook:  ".json_decode(utf8_decode($error_msg),true)['error_message'];
+        $token['error_msg'] = 'Tài khoản bị check point';
         exit(json_encode($token));
     }
 }
 else{
-    $token['error_msg'] = $data['error'];
+    $token['error_msg'] = 'Sai tài khoản,mật khẩu hoặc nick bị checkpoint';
     exit(json_encode($token));
 }
 unlink($random);
